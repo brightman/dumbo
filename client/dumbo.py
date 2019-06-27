d7cf7ea302726e6c65d26990dc105e244765d007
fengyong <fengyong@bendiyun.cn>
2019-06-27 19:11:08 +0800

    add BasicAuthServer

---
+++ client/dumbo.py
@@ -0,0 +1,183 @@
+__version__ = "0.1"
+
+import os,sys
+from functools import partial
+import urllib.parse
+import pdb
+from http import HTTPStatus
+from http.server import ThreadingHTTPServer,BaseHTTPRequestHandler,SimpleHTTPRequestHandler
+
+
+class BaiscAuthRequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, directory=None,basicauth="admin:admin", **kwargs):
+        if directory is None:
+            directory = os.getcwd()
+        self.directory = directory
+        self.basic_auth = basicauth
+        super().__init__(directory=directory,*args, **kwargs)
+    
+    def do_GET(self):
+        """Serve a GET request."""
+        print(self.path)
+        authorization = self.headers.get("authorization")
+        if authorization:
+            print(authorization)
+            auth = authorization.split()
+            import base64
+            auth = auth[1].encode('ascii')
+            str1 = base64.decodebytes(auth).decode('ascii')
+            #str1 = base64.b64decode(auth[1])
+            print(str1)
+            #user,password = str1.split(':')
+            if str1 == self.basic_auth:
+                f = self.send_head()
+                if f:
+                    try:
+                        self.copyfile(f, self.wfile)
+                    finally:
+                        f.close()
+            else:
+                self.do_AUTH()
+                '''
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                r = "The username or password is not correct!\r\n"
+                self.wfile.write(b"<h2>The username or password is not correct!</h2>\r\n")
+                '''
+        else:
+            self.do_AUTH()
+
+    def do_AUTH(self):
+        self.send_response(401)
+        self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def send_head(self):
+        """Common code for GET and HEAD commands.
+
+        This sends the response code and MIME headers.
+
+        Return value is either a file object (which has to be copied
+        to the outputfile by the caller unless the command was HEAD,
+        and must be closed by the caller under all circumstances), or
+        None, in which case the caller has nothing further to do.
+
+        """
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            parts = urllib.parse.urlsplit(self.path)
+            if not parts.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(HTTPStatus.MOVED_PERMANENTLY)
+                new_parts = (parts[0], parts[1], parts[2] + '/',
+                             parts[3], parts[4])
+                new_url = urllib.parse.urlunsplit(new_parts)
+                self.send_header("Location", new_url)
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            self.send_error(HTTPStatus.NOT_FOUND, "File not found")
+            return None
+
+        try:
+            fs = os.fstat(f.fileno())
+            # Use browser cache if possible
+            if ("If-Modified-Since" in self.headers
+                    and "If-None-Match" not in self.headers):
+                # compare If-Modified-Since and time of last file modification
+                try:
+                    ims = email.utils.parsedate_to_datetime(
+                        self.headers["If-Modified-Since"])
+                except (TypeError, IndexError, OverflowError, ValueError):
+                    # ignore ill-formed values
+                    pass
+                else:
+                    if ims.tzinfo is None:
+                        # obsolete format with no timezone, cf.
+                        # https://tools.ietf.org/html/rfc7231#section-7.1.1.1
+                        ims = ims.replace(tzinfo=datetime.timezone.utc)
+                    if ims.tzinfo is datetime.timezone.utc:
+                        # compare to UTC datetime of last modification
+                        last_modif = datetime.datetime.fromtimestamp(
+                            fs.st_mtime, datetime.timezone.utc)
+                        # remove microseconds, like in If-Modified-Since
+                        last_modif = last_modif.replace(microsecond=0)
+
+                        if last_modif <= ims:
+                            self.send_response(HTTPStatus.NOT_MODIFIED)
+                            self.end_headers()
+                            f.close()
+                            return None
+
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", "application/octet-stream")
+            self.send_header("Content-Disposition", "attachment; filename=\"%s\"" % path.split('/')[-1])
+
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified",
+                self.date_time_string(fs.st_mtime))
+            self.end_headers()
+            return f
+        except:
+            f.close()
+            raise
+
+def test(HandlerClass=BaseHTTPRequestHandler,
+         ServerClass=ThreadingHTTPServer,
+         protocol="HTTP/1.0", port=8000, bind=""):
+    """Test the HTTP request handler class.
+
+    This runs an HTTP server on port 8000 (or the port argument).
+
+    """
+    server_address = (bind, port)
+
+    HandlerClass.protocol_version = protocol
+    with ServerClass(server_address, HandlerClass) as httpd:
+        sa = httpd.socket.getsockname()
+        serve_message = "Serving HTTP on {host} port {port} (http://{host}:{port}/) ..."
+        print(serve_message.format(host=sa[0], port=sa[1]))
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt received, exiting.")
+            sys.exit(0)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--auth', '-a', default='admin:admin', metavar='AUTH',
+                        help='Baisc Auth username:password '
+                             '[user: password]')
+    parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
+                        help='Specify alternate bind address '
+                             '[default: all interfaces]')
+    parser.add_argument('--directory', '-d', default=os.getcwd(),
+                        help='Specify alternative directory '
+                        '[default:current directory]')
+    parser.add_argument('port', action='store',
+                        default=8000, type=int,
+                        nargs='?',
+                        help='Specify alternate port [default: 8000]')
+    args = parser.parse_args() 
+    handler_class = partial(BaiscAuthRequestHandler,
+                                directory=args.directory,basicauth=args.auth)
+    
+    print("\nDir: %s" % args.directory)
+    print("Basic Auth: %s\n" % args.auth)
+
+    test(HandlerClass=handler_class, port=args.port, bind=args.bind)
